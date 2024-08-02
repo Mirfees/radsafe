e07781c8c50c8102f81fcc28f37b4358cfec437b
Miron Fissenko <mirfees@gmail.com>
2024-08-02 18:36:45 +0500

    Закончил адаптив

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,13 @@
 document.addEventListener('DOMContentLoaded', function () {
+    // burger-menu begin
+    let burgerMenu = document.getElementById('burger-menu');
+    let overlay = document.querySelector('.header-menu');
+    burgerMenu.addEventListener('click',function(){
+        this.classList.toggle("close");
+        overlay.classList.toggle("overlay");
+    });
+    // burger-menu end
+
     // fancybox begin
     Fancybox.bind("[data-fancybox]", {
         // Your custom options
@@ -18,6 +27,20 @@ document.addEventListener('DOMContentLoaded', function () {
             nextEl: '.swiper-button-next',
             prevEl: '.swiper-button-prev',
         },
+
+        breakpoints: {
+            0: {
+                slidesPerView: 1
+            },
+
+            762: {
+                slidesPerView: 2
+            },
+
+            992: {
+                slidesPerView: 3
+            }
+        }
     });
 
     const pageInnerSlider = new Swiper('.page-inner__slider', {
@@ -44,6 +67,24 @@ document.addEventListener('DOMContentLoaded', function () {
             nextEl: '.swiper-button-next',
             prevEl: '.swiper-button-prev',
         },
+
+        breakpoints: {
+            0: {
+                slidesPerView: 1
+            },
+
+            768: {
+                slidesPerView: 2
+            },
+
+            992: {
+                slidesPerView: 3
+            },
+
+            1200: {
+                slidesPerView: 4
+            }
+        }
     });
 
    // sliders end
